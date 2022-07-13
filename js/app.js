7b58f81eeb83832ae35e175b9486462836122c48
nard.calma@gmail.com <nard.calma@gmail.com>
2022-07-12 18:28:26 -0700

    canvas added with border to match whole screen

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// canvas references
+// tutorial https://www.youtube.com/c/ChrisCourses
+// 
+
+
+const canvas = document.querySelector("canvas")
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight
