8b44d4a97be82eb2b23174ec5a7133bea73cd616
nard.calma@gmail.com <nard.calma@gmail.com>
2022-07-13 20:46:05 -0700

    added player class and game class

--- js/app.js
+++ js/app.js
@@ -10,11 +10,46 @@ const canvas = document.querySelector("canvas")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+//grab canvas context
+const context = canvas.getContext("2d");
 
-
-playerIcon = canvas.getContext("2d");
+//variables
+const startPlayerPosX = canvas.width/10
+const startPlayerPosY = canvas.height/2
 
 //player info
+class Player {
+    constructor() {
+        this.name = "floatshoe";
+        this.imgSrc = "images/shoe_icon.png";
+        this.posX = 0;
+        this.posY = 0;
+    }
+}
+
+class Game {
+    constructor(player) {
+        this.player = "";
+    }
+    //create player function
+    createPlayer = () => {
+        this.player = new Player();
+        console.log("Player is created.")
+    }
+    // add image to screen
+    addImage = (object,posX,posY) => {
+        // console.log("inside addImage",object)
+        object.posX = posX;
+        object.posY = posY;
+        const image = new Image();
+        image.src = object.imgSrc;
+        image.onload = () => {
+            context.drawImage(image,object.posX,object.posY)
+        }
+        console.log(object.name,"is added to the screen")
+    }
+}
+
 //starting x 
 const startingX = canvas.width/10;
 const startingY = canvas.height/2;
@@ -37,9 +72,8 @@ addPlayerIcon(startingX,startingY);
 
 //move player function
 // player move up
-const movePlayerUp = (posX,posY) => {
-    let oldX = posX;
-    let oldY = posY;
+const movePlayerUp = (player) => {
+    let positionY = player.posY
     positionPlayerY = oldY - 10;
     imgNew = new Image();
     imgNew.src = shoeIconPath;
@@ -77,9 +111,6 @@ document.addEventListener("keyup",(e)=>{
     }
 })
 
-
-
-
 // // player class
 // class Player {
 //     // has x and y axis
@@ -107,7 +138,7 @@ document.addEventListener("keyup",(e)=>{
 //     },
 //  }
 // //add player to game
-// //add obstacle to the game
+// 
 // // background can move 
 
 // // obstacle class
@@ -118,3 +149,12 @@ document.addEventListener("keyup",(e)=>{
 // player = new Player();
 // // console.log(Game.createPlayer)
 // Game.addPlayer(player)
+
+
+
+//create player
+const game = new Game()
+
+game.createPlayer()
+console.log(game.player)
+game.addImage(game.player,startPlayerPosX,startPlayerPosY)
